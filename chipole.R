7b53645eac28b904a36ef7c801ab6bf786b2c65c
Allen Xu <ziyuexu2021@u.northwestern.edu>
2020-09-08 15:04:45 -0500

    first section: chipole count by state

--- chipole.R
+++ chipole.R
@@ -1,3 +1,24 @@
 library(tidyverse)
+library(ggplot2)
+library(usmap)
 
+# read date
 location <- read_csv("chipotle_stores.csv")
+
+
+# I. over look of chipole count by state ----------------------------------
+
+count <- location %>%
+    group_by(state) %>%
+    tally()
+colnames(count) <- c("full", "count")
+
+data(statepop)
+
+count <- count %>%
+    left_join(statepop, by = "full")
+count <- count[,1:4]
+
+
+plot_usmap(data = count, values = "count") + scale_fill_continuous(low = "white", high = "red", name = "count", label = scales::comma) + theme(legend.position = "right")
+
